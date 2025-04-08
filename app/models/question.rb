365d8b4344e74d54fd344a2b3595efbd85d396ce
Mao Kaneda <maddieqqie@gmail.com>
2025-04-08 20:39:08 +0900

    若干修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
+  
+  validates :title, presence: { message: 'タイトルを入力してください' }
+  validates :content, presence: { message: '質問内容を入力してください' }
+  validates :author, presence: { message: '投稿者名を入力してください' }
 end
